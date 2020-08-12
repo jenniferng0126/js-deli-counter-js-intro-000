76a6768f705c82b98f3b051bdc16f3925c53fd67
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 16:58:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currLine, name) {
   currLine.push(name);
-  return `Welcome, ${name}. You are number ${currLine.length + 1} in line.`;
+  return `Welcome, ${name}. You are number ${currLine.length} in line.`;
 }
