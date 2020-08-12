44170ee33666106a0dd6e6c539ce1a949d531dac
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 17:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(currLine, name) {
   currLine.push(name);
   return `Welcome, ${name}. You are number ${currLine.length} in line.`;
 }
+
+function nowServing(currLine) {
+  if (currLine.length === 0) {
+    "There is nobody waiting to be served!";
+  } else {
+    let firstPerson = currLine.shift();
+    return firstPerson;
+  }
+}
