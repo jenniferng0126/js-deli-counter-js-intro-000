23aa42ec8b6414c66b2a346f98aca15c57ec49c7
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 17:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(currLine, name) {
 
 function nowServing(currLine) {
   if (currLine.length === 0) {
-    "There is nobody waiting to be served!";
+    return "There is nobody waiting to be served!";
   } else {
     let firstPerson = currLine.shift();
     return `Currently serving ${firstPerson}.`;
